0f71d2d7c332ef31ff5010301258f0275a12bc25
Luis Miguens Fernandez <luis.miguens@cern.ch>
2024-08-28 09:22:13 +0200

    Improve import mechanishm for python tests

--- test/python/common.py
+++ test/python/common.py
@@ -1,6 +1,11 @@
 import sys
+import os
 
 # Add the path to the directory containing the module
-sys.path.append("build/")
+os.environ["LD_LIBRARY_PATH"] = (
+    os.path.join(os.getcwd(), "build") + ":" + os.environ.get("LD_LIBRARY_PATH", "")
+)
+
+sys.path.append(os.path.join(os.getcwd(), "build"))
 
 from canmodule import *
